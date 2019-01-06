1960b3951a9330640ccdac8667bbad15a0292b26
NlionDev <lion.nicolas.dev@gmail.com>
2019-01-06 17:22:19 +0100

    Première ebauche de l'étape 1

---
+++ Project5/Instagrid/Instagrid/Model/ButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonView.swift
+//  Instagrid
+//
+//  Created by Nicolas Lion on 02/01/2019.
+//  Copyright © 2019 Nicolas Lion. All rights reserved.
+//
+
+import UIKit
+
+class ButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
